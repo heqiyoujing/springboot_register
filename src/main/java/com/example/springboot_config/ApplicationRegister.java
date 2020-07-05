1b47ce4d845ad3886b63de02742cb8f585881cca
heqiyoujing <38516694+heqiyoujing@users.noreply.github.com>
2020-07-05 15:02:09 +0800

    Update ApplicationRegister.java
    启动排除扫描datasource

--- src/main/java/com/example/springboot_config/ApplicationRegister.java
+++ src/main/java/com/example/springboot_config/ApplicationRegister.java
@@ -2,10 +2,11 @@ package com.example.springboot_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ApplicationRegister {
 
 	public static void main(String[] args) {
